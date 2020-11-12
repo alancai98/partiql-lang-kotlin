369ed9f35e932d6d6e56afd7e3d2987800feaa4e
Alan Cai <caialan@amazon.com>
2020-11-11 19:12:33 -0500

    Add one test

---
+++ lang/test/org/partiql/lang/DivideTest.kt
@@ -0,0 +1,16 @@
+package org.partiql.lang
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class DivideTest {
+    @Test
+    fun validDivideTest() {
+        assertEquals(2, Divide().divide(4, 2))
+    }
+
+//    @Test
+//    fun invalidDivideTest() {
+//        assertEquals(0, Divide().divide(4, 0))
+//    }
+}
